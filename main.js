1d85447ffd35db1d7e8c4461ceaaf9e26fd01295
Tiago Lisboa <129691470+Lisboa06@users.noreply.github.com>
2024-05-26 20:48:47 -0300

    modificações gerais
    "image comparasion" adicionado

--- main.js
+++ main.js
@@ -7,3 +7,12 @@ document.addEventListener("scroll", function() {
     //   document.querySelector(".container-fluid").classList.remove("menu-icon-onScroll");
     }
 });
+
+const slider = document.querySelector(".slider input");
+const img = document.querySelector(".images .img-2");
+const dragLine = document.querySelector(".slider .drag-line");
+slider.oninput = ()=>{
+  let sliderVal = slider.value;
+  dragLine.style.left = sliderVal + "%";
+  img.style.width = sliderVal + "%";
+}
