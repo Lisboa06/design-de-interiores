fc39a986ef50b58feea2e2f1decf7622fca0e5f0
Lisboa06 <tiagolisboa2020@gmail.com>
2024-05-27 16:08:07 -0300

    modificações nos estilos e cores da página

--- main.js
+++ main.js
@@ -8,6 +8,20 @@ document.addEventListener("scroll", function() {
     }
 });
 
+var navLinks = document.querySelectorAll('.nav-link a[href^="#"]');
+navLinks.forEach(function(link) {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    var id = this.getAttribute('href');
+    var targetOffset = document.querySelector(id).offsetTop;
+    
+    window.scrollTo({
+      top: targetOffset - 105,
+      behavior: 'smooth'
+    });
+  });
+});
+
 const slider = document.querySelector(".slider input");
 const img = document.querySelector(".images .img-2");
 const dragLine = document.querySelector(".slider .drag-line");
